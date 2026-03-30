e01da21c3cbd9689e5eec56fef7e7b75d5a8746a
Akira <matheus77akiraaso@gmail.com>
2026-03-30 09:34:13 -0300

    feat: Operadores Aritiméticos e Lógicos

--- src/js/script.js
+++ src/js/script.js
@@ -42,3 +42,26 @@ console.log(exemplo7)
 //valor vazio
 let exemplo8 =null;
 console.log(exemplo8)
+
+//OPERADORES ARITIMÉTICOS
+
+const valor1=10;
+const valor2=20;
+
+console.log( valor1 + valor2);
+console.log( valor1 - valor2);
+console.log( valor1 * valor2);
+console.log( valor1 / valor2);
+
+//OPERADORES LÓGICOS
+// = atribução
+// == comparação
+// === valor e tipo(estritamente igual)
+// && = E (todas condições tem que ser verdadeiras)
+// || = OU (uma das condição tem que ser verdadeira)
+
+console.log(valor1 < valor2);
+console.log(valor1 > 10 && valor2< 10);
+console.log(valor2 < 100 || valor1 >10);
+console.log(valor1 == valor2);
+console.log(valor2 === valor1);
